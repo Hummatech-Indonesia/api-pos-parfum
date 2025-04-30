c577053235d0ef3c2f34ce130bdb10149a636bcd
putrapangestu <putraprp8@gmail.com>
2025-04-30 10:42:23 +0700

    FEATURE TRANSAKSI MAPPING STORE

--- app/Services/Transaction/TransactionService.php
+++ app/Services/Transaction/TransactionService.php
@@ -14,8 +14,7 @@ class TransactionService
     {
         try {
             return [
-                'stpre_id' => auth()->user()?->store_id ?? auth()->user()?->store?->id,
-                'stpre_id' => auth()->user()?->store_id ?? auth()->user()?->store?->id,
+                'store_id' => auth()->user()?->store_id ?? auth()->user()?->store?->id,
                 'transaction_status' => "Success",
                 'user_id' => isset($data["user_id"]) ? $data["user_id"] : null,
                 'user_name' => isset($data["user_name"]) ? $data["user_name"] : null,
@@ -24,7 +23,8 @@ class TransactionService
                 'amount_tax' => isset($data["amount_tax"]) ? $data["amount_tax"] : null,
                 'total_price' => isset($data["total_price"]) ? $data["total_price"] : null,
                 'payment_method' => isset($data["payment_method"]) ? $data["payment_method"] : null,
-                'note' => isset($data["note"]) ? $data["note"] : null
+                'note' => isset($data["note"]) ? $data["note"] : null,
+                'payment_time' => now()
             ];
         }catch(\Throwable $th){
             Log::error($th->getMessage());
