a2287152c43c7e7efaffc6a75609fe2e0375f637
athasyah <athasyahaddin24@gmail.com>
2025-06-25 10:22:44 +0700

    fix productDetailrepo

--- app/Contracts/Repositories/Master/ProductDetailRepository.php
+++ app/Contracts/Repositories/Master/ProductDetailRepository.php
@@ -28,7 +28,7 @@ class ProductDetailRepository extends BaseRepository implements ProductDetailInt
     {
         return $this->model->query()
             ->withCount('product')
-            ->with('varian', 'product.productBundling.details', 'category', 'productStockOutlet', 'productStockWarehouse')
+            ->with( 'product.productBundling.details', 'category', 'productStockOutlet', 'productStockWarehouse')
             ->when(count($data) > 0, function ($query) use ($data) {
                 foreach ($data as $index => $value) {
                     $query->where($index, $value);
@@ -40,7 +40,7 @@ class ProductDetailRepository extends BaseRepository implements ProductDetailInt
     {
         return $this->model->query()
             ->withCount('product')
-            ->with('varian', 'product.productBundling.details', 'category', 'productStockOutlet', 'productStockWarehouse')
+            ->with( 'product.productBundling.details', 'category', 'productStockOutlet', 'productStockWarehouse')
             ->when(count($data) > 0, function ($query) use ($data) {
                 if (isset($data["search"])) {
                     $query->where(function ($query2) use ($data) {
@@ -59,12 +59,12 @@ class ProductDetailRepository extends BaseRepository implements ProductDetailInt
 
     public function show(mixed $id): mixed
     {
-        return $this->model->with('varian', 'product', 'category')->find($id);
+        return $this->model->with( 'product', 'category')->find($id);
     }
 
     public function checkActive(mixed $id): mixed
     {
-        return $this->model->with('varian', 'product', 'category')->where('is_delete', 0)->find($id);
+        return $this->model->with( 'product', 'category')->where('is_delete', 0)->find($id);
     }
 
     public function update(mixed $id, array $data): mixed
