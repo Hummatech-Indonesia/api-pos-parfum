5cdd1a0eeffa2416e766e316595370e06b6e1d48
chiillguy <elfafaza03@gmail.com>
2025-06-25 10:35:13 +0700

    update product blend resource

--- app/Http/Resources/ProductBlendResource.php
+++ app/Http/Resources/ProductBlendResource.php
@@ -15,6 +15,7 @@ class ProductBlendResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'blend_name' => $this->product->blend_name ?? $this->product->name ?? null,
             'quantity' => $this->result_stock,
             'description' => $this->description,
