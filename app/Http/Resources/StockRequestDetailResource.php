e3df4c2637f25932c5771c82d0234484b4cb1c74
athasyah <athasyahaddin24@gmail.com>
2025-07-07 19:07:41 +0700

    fix stock request

--- app/Http/Resources/StockRequestDetailResource.php
+++ app/Http/Resources/StockRequestDetailResource.php
@@ -10,13 +10,13 @@ class StockRequestDetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'product_name' => optional($this->detailProduct->product)->name,
+            'product_name' => optional($this->detailProduct?->product)->name,
             'variant_name' => optional($this->detailProduct)->variant_name,
             'requested_stock' => $this->requested_stock,
-            'kategori' => optional($this->detailProduct->product->category)->name,
+            'kategori' => optional($this->detailProduct->product?->category)->name,
             'variant_code' => optional($this->detailProduct)->product_code,
-            'unit_id' => $this->unitRelation->id,
-            'unit_code' => $this->unitRelation->code
+            'unit_id' => $this->unitRelation?->id,
+            'unit_code' => $this->unitRelation?->code
         ];
     }
 }
