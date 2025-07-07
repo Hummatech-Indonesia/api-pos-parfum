0d133840d39a6dbcb587b07ce21842278cd139f5
YanceBerlian <berlianyance@gmail.com>
2025-07-07 08:08:52 +0700

    Fix Update User

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'image' => $this->image,
             'created_at' => $this->created_at,
             'roles' => $this->roles->pluck('name'),
         ];
